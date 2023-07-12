316ac2578d1632eefbce50a2c0dbf6d6e6d3a138
jeongyeham <35221897+jeongyeham@users.noreply.github.com>
2023-07-12 10:34:16 +0800

    Update Update_IDF.ps1

--- Firmware_Prj/Update_IDF.ps1
+++ Firmware_Prj/Update_IDF.ps1
@@ -1,7 +1,15 @@
+echo "-------------------Checking idf-components-------------------"
 cd  $ENV:IDF_PATH
 git checkout master
 git pull
 git submodule update --init --recursive
-& ../../Initialize-Idf.ps1
 & ./install.ps1
-cd  -
+
+echo "-------------------Checking idf-extra-components-------------------"
+cd  $ENV:IDF_EXTRA_PATH
+git checkout master
+git pull
+git submodule update --init --recursive
+
+cd - 
+cd -
